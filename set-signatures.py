9996797832774a0d4aa0def7e7f7f3238ee61c53
aungthuheinoo <aungthuheinoo1992@gmail.com>
2019-08-27 10:16:45 +0630

    first commit

--- set-signatures.py
+++ set-signatures.py
@@ -14,26 +14,26 @@ scopes = ['https://www.googleapis.com/auth/gmail.settings.basic']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('keyfile.json', scopes)
 
 def change_signature(email, name, title):
-    delegated_credentials = credentials.create_delegated(email)
-    http_auth = delegated_credentials.authorize(Http())
-    gmail = build('gmail', 'v1', http=http_auth)
-    send_as_body = {
-        'signature': Renderer().render_path('template.mustache', {
-            'name': name,
-            'title': title
-        })
-    }
+	delegated_credentials = credentials.create_delegated(email)
+	http_auth = delegated_credentials.authorize(Http())
+	gmail = build('gmail', 'v1', http=http_auth)
+	send_as_body = {
+		'signature': Renderer().render_path('template.mustache', {
+			'name': name,
+			'title': title
+		})
+	}
 
-    print('Changing signature for %s' % email)
+	print('Changing signature for %s' % email)
 
-    gmail.users().settings().sendAs().update(
-        userId='me',
-        sendAsEmail=email,
-        body=send_as_body
-    ).execute()
+	gmail.users().settings().sendAs().update(
+		userId='me',
+		sendAsEmail=email,
+		body=send_as_body
+	).execute()
 
-with open('users.csv', 'rb') as f:
-    reader = csv.reader(f)
-    next(reader, None)
-    for row in reader:
-        change_signature(row[0], row[1], row[2])
+with open('users.csv', 'r') as f:
+	reader = csv.reader(f)
+	next(reader, None)
+	for row in reader:
+		change_signature(row[0], row[1], row[2])
